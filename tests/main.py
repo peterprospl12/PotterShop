fcfa08e9aed4e6ece606bddb08ce6bd891709fdc
Piotr Sulewski <48958687+peterprospl12@users.noreply.github.com>
2024-12-09 13:11:25 +0100

    updatet test (#65)

--- tests/main.py
+++ tests/main.py
@@ -9,39 +9,49 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-website_addr = "http://localhost:8080/"
+
+TIME_SLEEP = 0
+
+
+website_addr = "https://localhost:8443/"
 service = Service(executable_path='/usr/bin/chromedriver')
-browser = webdriver.Chrome(service=service, options=Options())
+
+
+chrome_options = Options()
+chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("--ignore-ssl-errors")
+
+browser = webdriver.Chrome(service=service, options=chrome_options)
 
 
 
 
 def Z1(website_addr, browser):
-    def products_links(url, ile):
+    def products_links(url):
         browser.get(url)
-        WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'product'))
-        )
+        time.sleep(TIME_SLEEP)
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'product')))
         products = browser.find_elements(By.CLASS_NAME, 'thumbnail')
         product_links = []
         for i in range(len(products)):
              product_links.append(products[i].get_attribute("href"))
-        return random.sample(product_links, k=ile)
+        return product_links
         
         
     def add_to_the_cart(url):
         browser.get(url)
+        time.sleep(TIME_SLEEP)
         WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'container')))
         try:
-            a = WebDriverWait(browser, 0.1).until(EC.element_to_be_clickable((By.CLASS_NAME, 'add-to-cart')))
+            a = WebDriverWait(browser, 0.1).until(EC.element_to_be_clickable((By.CLASS_NAME, 'product-page-add-to-cart')))
         except TimeoutException:
-            return
+            return 1
         try:
             WebDriverWait(browser, 0.1).until(EC.presence_of_element_located((By.XPATH, "//span[@data-stock]")))
             dostepne = browser.find_element(By.XPATH, "//span[@data-stock]")
             dostepne = dostepne.get_attribute("data-stock")
         except TimeoutException:
-            dostepne = 10
+            dostepne = 2
         b = random.randint(1, int(dostepne))
         
         D_ile = browser.find_element(By.NAME, 'qty')
@@ -50,62 +60,72 @@ def Z1(website_addr, browser):
         B_dodaj = browser.find_element(By.CLASS_NAME, 'add-to-cart')
         B_dodaj.click()
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'cart-products-count')))
+        return 2
         
         
     ile = random.randint(1, 10)
     ile2 = [ile, 10-ile]
-    Products_links1 = products_links(website_addr + '25-ksiazki', ile2[0])
-    Products_links2 = products_links(website_addr + '18-kubki', ile2[1])
+    Products_links1 = products_links(website_addr + '72-ksiazki')
+    Products_links2 = products_links(website_addr + '65-kubki')
     for link in Products_links1:
-        add_to_the_cart(link)
+        if add_to_the_cart(link) == 2: ile2[0]-=1
+        if ile2[0] == 0: break
     for link in Products_links2:
-        add_to_the_cart(link)
+        if add_to_the_cart(link) == 2: ile2[1]-=1
+        if ile2[1] == 0: break
+    time.sleep(TIME_SLEEP)
 
 
 
 
 
 def Z2(website_addr, browser, name):
-    def get_products(name):
-        I_search = browser.find_element(By.NAME, 's')
-        I_search.send_keys(name)
-        I_search.send_keys(Keys.RETURN)
-        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'product')))
-        
-        products = browser.find_elements(By.CLASS_NAME, 'thumbnail')
-        dostepne_produkty = []
-        for idx in range(len(products)):
-             dostepne_produkty.append(products[idx])
-        return dostepne_produkty
-
-
     def add_to_the_cart(products):
-        product = random.choice(products)
-        product.click()
+        browser.get(random.choice(products))
+        time.sleep(TIME_SLEEP)
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'container')))
+
+        try:
+            a = WebDriverWait(browser, 0.1).until(EC.element_to_be_clickable((By.CLASS_NAME, 'product-page-add-to-cart')))
+        except TimeoutException:
+            add_to_the_cart(products)
+            return
+        
         B_dodaj = browser.find_element(By.CLASS_NAME, 'add-to-cart')
         B_dodaj.click()
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'cart-products-count')))
 
-
     browser.get(website_addr)
-    products = get_products(name)        
-    add_to_the_cart(products)
+    I_search = browser.find_element(By.NAME, 's')
+    I_search.send_keys(name)
+    I_search.send_keys(Keys.RETURN)
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'product')))
+    time.sleep(TIME_SLEEP)
+    
+    products = browser.find_elements(By.CLASS_NAME, 'thumbnail')
+    dostepne_produkty = []
+    for i in range(len(products)):
+        dostepne_produkty.append(products[i].get_attribute("href"))
+    add_to_the_cart(dostepne_produkty)
 
             
         
 
 
 def Z3(website_addr, browser):
-    def links_to_delete():
-        browser.get(website_addr)
-        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'remove-from-cart')))
-        products = browser.find_elements(By.CLASS_NAME, 'remove-from-cart')
-        product_links = [element.get_attribute("href") for element in products]
-        return random.sample(product_links, k=3)
-
-    links = links_to_delete()
+    browser.get(website_addr)
+    time.sleep(TIME_SLEEP)
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'remove-from-cart')))
+    products = browser.find_elements(By.CLASS_NAME, 'remove-from-cart')
+    product_links = [element.get_attribute("href") for element in products]
+    jest = len(product_links)
+    if jest <= 3: Z = jest-1
+    else: Z = 3
+    links = random.sample(product_links, k=Z)
+    
     for link in links:
         browser.get(link)
+        time.sleep(TIME_SLEEP)
 
 
 
@@ -113,14 +133,16 @@ def Z3(website_addr, browser):
 
 def Z4(website_addr, browser):
     browser.get(website_addr)
+    time.sleep(TIME_SLEEP)
     WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'form-control-submit')))
     browser.find_element(By.CSS_SELECTOR, "input[value=\"1\"]").click()
     browser.find_element(By.CSS_SELECTOR, "input[name=\"firstname\"]").send_keys("Name")
     browser.find_element(By.CSS_SELECTOR, "input[name=\"lastname\"]").send_keys("Surname")
-    browser.find_element(By.CSS_SELECTOR, "input[name=\"email\"]").send_keys("test" + str(random.randint(0, 999)) + "@gmail.com")
+    browser.find_element(By.CSS_SELECTOR, "input[name=\"email\"]").send_keys("test" + str(random.randint(0, 99999999)) + "@gmail.com")
     browser.find_element(By.CSS_SELECTOR, "input[name=\"password\"]").send_keys("password")
     browser.find_element(By.CSS_SELECTOR, "input[name=\"customer_privacy\"]").click()
     browser.find_element(By.CSS_SELECTOR, "input[name=\"psgdpr\"]").click()
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.CLASS_NAME, 'form-control-submit').click()
 
 
@@ -129,20 +151,25 @@ def Z4(website_addr, browser):
 
 def Z5(website_addr, browser):
     browser.get(website_addr)
+    time.sleep(TIME_SLEEP)
 
     browser.find_element(By.ID, 'field-address1').send_keys('ADDRESS')
     browser.find_element(By.ID, 'field-postcode').send_keys('00-000')
-    browser.find_element(By.ID, 'field-city').send_keys('CITY')    
+    browser.find_element(By.ID, 'field-city').send_keys('CITY')
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.NAME, 'confirm-addresses').click()
     WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'continue')))
 
     browser.find_element(By.ID, 'delivery_option_24').click()
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.NAME, 'confirmDeliveryOption').click()
     WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'payment-confirmation')))
 
     browser.find_element(By.ID, 'payment-option-2').click()
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.ID, 'conditions_to_approve[terms-and-conditions]').click()
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary.center-block")))
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.CSS_SELECTOR, ".btn.btn-primary.center-block").click()
 
 
@@ -153,12 +180,15 @@ def Z6(website_addr, browser):
     browser.get(website_addr)
         
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'account')))
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.CLASS_NAME, 'account').click()
 
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'history-link')))
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.ID, 'history-link').click()
 
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[data-link-action=\"view-order-details\"]")))
+    time.sleep(TIME_SLEEP)
     browser.find_element(By.CSS_SELECTOR, "a[data-link-action=\"view-order-details\"]").click()
 
 
@@ -169,10 +199,10 @@ def Z6(website_addr, browser):
         try:
             a = element.find_element(By.TAG_NAME, 'a')
             browser.get(a.get_attribute('href'))
+            time.sleep(TIME_SLEEP)
             return
         except NoSuchElementException as e:
             continue
-        #print("Brak faktury.")
 
 
 
