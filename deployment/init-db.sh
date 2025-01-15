cbcb2401b912d31db7cc62074066b93245ad4609
tomasz <sankowski.tomek@gmail.com>
2025-01-15 22:12:36 +0100

    Deployment dir update

--- deployment/init-db.sh
+++ deployment/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Konfiguracja dla importu bazy danych
-DB_HOST="admin-mysql_db"        # Host bazy danych (nazwa kontenera)
+DB_HOST="admin-mysql_db.1.l98q1e4h4cnparwuxzj1eqk2r"        # Host bazy danych (nazwa kontenera)
 DB_USER="root"                  # Nazwa użytkownika bazy danych
 DB_PASS="student"               # Hasło do bazy danych
 DB_NAME="BE_188667"             # Nazwa bazy danych do utworzenia
@@ -12,6 +12,9 @@ if [ ! -f "$DUMP_FILE" ]; then
     echo "Plik dump $DUMP_FILE nie istnieje."
     exit 1
 fi
+
+# Usunięcie tabeli, jeśli istnieje
+docker exec -i $DB_HOST mysql -u "$DB_USER" -p"$DB_PASS" -e "USE $DB_NAME; DROP TABLE IF EXISTS $TABLE_NAME;"
 
 # Tworzenie bazy danych, jeśli nie istnieje
 docker exec -i $DB_HOST mysql -u "$DB_USER" -p"$DB_PASS" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
@@ -26,4 +29,5 @@ else
     echo "Wystąpił błąd przy imporcie bazy danych."
     exit 1
 fi
+
 
